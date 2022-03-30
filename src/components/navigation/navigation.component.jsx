3b08680db1dcb2164c35b5f800b973153becdc42
Julien Fleury <julienfleury93@gmail.com>
2022-03-30 09:03:51 +0200

    Adding basic React setup as well as adding the Navigation component frame.

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return(
+        <div className="navigationContainer">
+
+        </div>
+    );
+}
+
+export default Navigation;
