68d09004095eae534c011ab72eb156c1c7f0c9f6
Julien Fleury <julienfleury93@gmail.com>
2022-03-30 11:43:48 +0200

    Added styles and svg icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import LoqualisLogo from './assets/loqualisLogo.png';
+import Navigation from './components/navigation/navigation.component';
 
 const App = () => {
   const testClick = () => {
@@ -23,7 +24,7 @@ const App = () => {
       </div>
       <div className="row mainInfoContainer">
         <div id="mainNav">
-
+          <Navigation />
         </div>
         <div id="mainInfo">
           <button onClick={testClick}>Test</button>
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import './navigation.styles.scss';
+import { ReactComponent as DashboardIcon } from '../../assets/dashboardIcon.svg';
 
 const Navigation = () => {
-    return(
+    return (
         <div className="navigationContainer">
+            <div className="dashBoardSelector navItems">
+                <DashboardIcon className="icon" />
+                <p>Dashboard</p>
+            </div>
 
         </div>
     );
