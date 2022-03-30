cb46e2bc2b064bbb2c0c990fdd61bfd37504feca
Julien Fleury <julienfleury93@gmail.com>
2022-03-30 12:05:07 +0200

    Updating all the SVG data to make everything the same.

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import './navigation.styles.scss';
+// Icons
 import { ReactComponent as DashboardIcon } from '../../assets/dashboardIcon.svg';
+import { ReactComponent as OffersIcon } from '../../assets/offersIcon.svg';
+import { ReactComponent as PartnersIcon } from '../../assets/partnersIcon.svg';
+import { ReactComponent as CommandesIcon } from '../../assets/commandesIcon.svg';
+import { ReactComponent as SettingsIcon } from '../../assets/settingsIcon.svg';
+import { ReactComponent as LogoutIcon } from '../../assets/logoutIcon.svg';
+
 
 const Navigation = () => {
     return (
         <div className="navigationContainer">
-            <div className="dashBoardSelector navItems">
+            <div className="navItems">
                 <DashboardIcon className="icon" />
                 <p>Dashboard</p>
             </div>
-
+            <div className="navItems">
+                <OffersIcon className="icon" />
+                <p>Offres</p>
+            </div>
         </div>
     );
 }
